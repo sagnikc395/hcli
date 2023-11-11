4e863a53b9f555511fa5559b7bea4405674d3cbb
sagnikc395 <sagnikchatterjee395@gmail.com>
2023-11-12 00:31:28 +0530

    src: need to render url on the input

--- main.go
+++ main.go
@@ -5,10 +5,71 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
+type (
+	errMsg error
+)
+
+type model struct {
+	textInput textinput.Model
+	err       error
+}
+
+func initialModel() model {
+	ti := textinput.New()
+	ti.Placeholder = "URL"
+	ti.Focus()
+	ti.CharLimit = 156
+	ti.Width = 20
+
+	return model{
+		textInput: ti,
+		err:       nil,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return textinput.Blink
+}
+func (m model) View() string {
+	return fmt.Sprintf(
+		"What’s your favorite Pokémon?\n\n%s\n\n%s",
+		m.textInput.View(),
+		"(esc to quit)",
+	) + "\n"
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyEnter, tea.KeyCtrlC, tea.KeyEsc:
+			return m, tea.Quit
+		}
+
+	// We handle errors just like any other message
+	case errMsg:
+		m.err = msg
+		return m, nil
+	}
+
+	m.textInput, cmd = m.textInput.Update(msg)
+	return m, cmd
+}
+
 func main() {
-	url := `https://hacker-news.firebaseio.com/v0/item/2921983.json?print=pretty`
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+	url := string(model.textInput)
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatalln(err)
